82205014db9384e1afcf75a0c552d883e2924a37
StrangePerch <sasha.aav.sv@gmail.com>
2020-09-02 20:47:09 +0300

    step save

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,21 +1,27 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "iostream.h"
+#include "string.h"
+#include "fstream.h"
 
-using namespace std;
-
-#include "Builder.h"
+#include "Composite.h"
 
 
 int main()
 {
-	MusicCenterBuilder a;
-	ManualBuilder c; 
-	Director b(c);
-	b.buildMaxiMC();
-	c.getResult()->print();
-	b.setBuilder(a);
-	b.buildMaxiMC();
-	a.getResult()->print();
+	Component* comp = new Folder("FileSystem");
+	Component* folder1 = new Folder("Folder1");
+	Component* folder2 = new Folder("Folder2");
+	Component* folder3 = new Folder("Folder3");
+	Component* file1 = new File("File1");
+	Component* file2 = new File("File2");
+	Component* file3 = new File("File3");
+	Component* file4 = new File("File4");
+	comp->add(folder1);
+	folder1->add(folder2);
+	folder1->add(folder3);
+	comp->add(file1);
+	folder2->add(file2);
+	folder3->add(file3);
+	folder3->add(file4);
+	comp->print();
 
 }
---
+++ Project/Project/fstream.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
