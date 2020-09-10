11f2ff648578427ab336e92a655e38c3915b93cc
StrangePerch <sasha.aav.sv@gmail.com>
2020-09-10 20:46:43 +0300

    save

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,10 +1,16 @@
+
+
 #include "iostream.h"
 #include "string.h"
 #include "fstream.h"
 
-
+#include "Template.h"
 
 int main()
 {
-
+	MegaHouse mh;
+	mh.start(8);
+	cout << endl << endl;
+	NormalHouse nh;
+	nh.start(4);
 }
